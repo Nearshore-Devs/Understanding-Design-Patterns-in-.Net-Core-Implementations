04da723a0d614df90d364befb61c0a83bde2bd39
devsnet-EdgarBringas <86986728+devsnet-EdgarBringas@users.noreply.github.com>
2021-07-05 17:06:04 -0600

    Command
    Initial idea for Command

--- Design_Patterns_Implementation/Program.cs
+++ Design_Patterns_Implementation/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Design_Patterns_Implementation
+namespace NearshoreDevs
 {
     public class Program
     {
--- Design_Patterns_Implementation/Startup.cs
+++ Design_Patterns_Implementation/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Design_Patterns_Implementation
+namespace NearshoreDevs
 {
     public class Startup
     {
