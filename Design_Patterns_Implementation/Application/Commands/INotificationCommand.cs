e325681afa060db1699d3d1f057472e912f32879
devsnet-EdgarBringas <86986728+devsnet-EdgarBringas@users.noreply.github.com>
2021-07-06 11:50:56 -0600

    Changes
    Added Interface and concrete implementations

---
+++ Design_Patterns_Implementation/Application/Commands/INotificationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NearshoreDevs.Application.Commands
+{
+    public interface INotificationCommand
+    {
+        void Execute(NotificationType type, string title, string body, IDictionary<string , string> extraData);
+    }
+
+
+    public enum NotificationType { Push, SMS, Mqtt};
+}
---
+++ Design_Patterns_Implementation/Application/Commands/PushNotificationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NearshoreDevs.Application.Commands
+{
+    public class PushNotificationCommand : INotificationCommand
+    {
+        public void Execute(NotificationType type, string title, string body, IDictionary<string, string> extraData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
